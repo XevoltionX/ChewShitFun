ce307c28c4018814c7a5cb332060fbdd003b8d39
余苟千 <13110713+xu-jingai@user.noreply.gitee.com>
2026-05-20 13:12:39 +0800

    feat: init Uniapp Vue3 project scaffold

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { onLaunch } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('🚽 ChewShitFun 启动')
+})
+</script>
+
+<style lang="scss">
+@import '@/uni.scss';
+</style>
